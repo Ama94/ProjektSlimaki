a2e872da621f6daf37b5dd9d1cb66c48582d4efd
Ssect0r <wojasinskipi@gmail.com>
2018-11-03 18:56:17 +0100

    First commit
    Created whole project, added working menu system with scenes switching. Added assets for snails

---
+++ Projekt Ślimaki/Assets/Scripts/Buttons Scripts/MainMenuButtonsScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class MainMenuButtonsScript : MonoBehaviour {
+
+public void changeSceneToQuickMatch()
+    {
+        SceneManager.LoadScene("QuickMatch", LoadSceneMode.Single);
+    }
+
+    public void changeSceneToSettigs()
+    {
+        SceneManager.LoadScene("Settings", LoadSceneMode.Single);
+    }
+
+    public void changeSceneToCredits()
+    {
+        SceneManager.LoadScene("Credits", LoadSceneMode.Single);
+    }
+
+    public void changeSceneToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
+
+    public void exitTheGame()
+    {
+        Application.Quit();
+    }
+ 
+
+	}
+
